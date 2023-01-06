06dae9b57353ebac8979f9a78f0d816bee3d1b82
davirany <davirany@gmail.com>
2023-01-05 23:46:03 -0300

    add test to SellsController

--- src/main/java/com/vehicles/controller/SellsController.java
+++ src/main/java/com/vehicles/controller/SellsController.java
@@ -1,6 +1,9 @@
 package com.vehicles.controller;
 
 import com.vehicles.model.Sell;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,9 +15,24 @@ public class SellsController {
     }
 
     public void createSell(Sell sell) {
-        /*TODO add an increment to advertiser and buyer*/
+        sell.getBuyer().setAmountBoughtVehicles(sell.getBuyer().getAmountBoughtVehicles() + 1);
+
+        sell.getAdvertising().getAdvertiser().setAmountActiveAds(sell.getAdvertising().getAdvertiser().getAmountActiveAds() - 1);
+
+        sell.getAdvertising().getAdvertiser().setAmountVehiclesSold(sell.getAdvertising().getAdvertiser().getAmountVehiclesSold() + 1);
+
+        sell.setDate(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")));
+
         sells.add(sell);
     }
 
-    /*TODO create a removeSell method*/
+    public void removeSell(Sell sell) {
+        sell.getBuyer().setAmountBoughtVehicles(sell.getBuyer().getAmountBoughtVehicles() - 1);
+
+        sell.getAdvertising().getAdvertiser().setAmountActiveAds(sell.getAdvertising().getAdvertiser().getAmountActiveAds() + 1);
+
+        sell.getAdvertising().getAdvertiser().setAmountVehiclesSold(sell.getAdvertising().getAdvertiser().getAmountVehiclesSold() - 1);
+
+        sells.remove(sell);
+    }
 }
--- src/test/java/com/vehicles/controller/SellsControllerTest.java
+++ src/test/java/com/vehicles/controller/SellsControllerTest.java
@@ -11,14 +11,36 @@ class SellsControllerTest {
 
     @Test
     void createSell() {
-        Advertiser advertiser = new Advertiser("name", "61912345678", "name@email.com", 0, 0);
+        Advertiser advertiser = new Advertiser("name", "61912345678", "name@email.com", 1, 0);
         Brand brand = new Brand("Chevrolet", "American", 1911);
         Model model = new Model("Camaro", brand);
         Vehicle vehicle = new Vehicle(model, 2022, 2023, "Yellow", 0, 550000, 1);
         Advertising advertising = new Advertising(1, "description", advertiser, vehicle);
-        Buyer buyer = new Buyer("buyerName", "61912345678", "email@email.com", 23);
+        Buyer buyer = new Buyer("buyerName", "61912345678", "email@email.com", 0);
         Sell sell = new Sell(advertising, "01/01/2001", buyer, 1);
         sellsController.createSell(sell);
+
         assertTrue(sellsController.getSells().contains(sell));
+        assertTrue(sell.getBuyer().getAmountBoughtVehicles() > 0);
+        assertTrue(sell.getAdvertising().getAdvertiser().getAmountActiveAds() < 1);
+        assertTrue(sell.getAdvertising().getAdvertiser().getAmountVehiclesSold() > 0);
+    }
+
+    @Test
+    void removeSell() {
+        Advertiser advertiser = new Advertiser("name", "61912345678", "name@email.com", 1, 0);
+        Brand brand = new Brand("Chevrolet", "American", 1911);
+        Model model = new Model("Camaro", brand);
+        Vehicle vehicle = new Vehicle(model, 2022, 2023, "Yellow", 0, 550000, 1);
+        Advertising advertising = new Advertising(1, "description", advertiser, vehicle);
+        Buyer buyer = new Buyer("buyerName", "61912345678", "email@email.com", 0);
+        Sell sell = new Sell(advertising, "01/01/2001", buyer, 1);
+        sellsController.createSell(sell);
+        sellsController.removeSell(sell);
+
+        assertFalse(sellsController.getSells().contains(sell));
+        assertEquals(0, sell.getBuyer().getAmountBoughtVehicles());
+        assertEquals(1, sell.getAdvertising().getAdvertiser().getAmountActiveAds());
+        assertEquals(0, sell.getAdvertising().getAdvertiser().getAmountVehiclesSold());
     }
 }
